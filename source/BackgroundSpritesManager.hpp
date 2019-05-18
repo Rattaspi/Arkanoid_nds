351cfc265f276e0b40e0ca60046746cb1083d1a4
Alex Canut/ASUS-DEV <a.canutperdigon@gmail.com>
2019-05-18 19:14:39 +0200

    3 clouds scrolling with different speeds.

--- source/BackgroundSpritesManager.hpp
+++ source/BackgroundSpritesManager.hpp
@@ -5,22 +5,46 @@
 
 class BackgroundSpritesManager{
 
-    BigSprite cloud;
-    std::pair<int, int> cloudPos = {SCREEN_WIDTH, (int)std::round(rand()%40) + 20 };
+    BigSprite cloud, cloud2, cloud3;
+    std::pair<int, int> cloudPos = {SCREEN_WIDTH, (int)std::round(rand()%80) + 10 };
+    std::pair<int, int> cloud2Pos = {(int)(rand()%SCREEN_WIDTH), (int)(rand()%80) + 20 };
+    std::pair<int, int> cloud3Pos = {(int)(rand()%SCREEN_WIDTH), (int)(rand()%80) + 20 };
 
     int speed = 1;
+
+    int cloud2FrameDelay = 3;
+    int cloud2FrameCounter = 0;
+    int cloud3FrameDelay = 9;
+    int cloud3FrameCounter = 0;
     
 public:
 
     BackgroundSpritesManager(){
         cloud = BigSprite(0, cloudPos.first, cloudPos.second, true);
+        cloud2 = BigSprite(0, cloud2Pos.first, cloud2Pos.second, true);
+        cloud3 = BigSprite(0, cloud3Pos.first, cloud3Pos.second, true);
     }
 
     void Update(){
+        //Cloud 1
         cloudPos.first -= speed;
+        //Cloud 2
+        if(cloud2FrameCounter > cloud2FrameDelay){
+            cloud2Pos.first -= speed;
+            cloud2FrameCounter = 0;
+        }
+        if(cloud3FrameCounter > cloud3FrameDelay){
+            cloud3Pos.first -= speed;
+            cloud3FrameCounter = 0;
+        }
+
+        cloud2FrameCounter++;
+        cloud3FrameCounter++;
     }
 
     void Draw(){
         cloud.PlaceSprite(cloudPos.first, cloudPos.second);
+        cloud2.PlaceSprite(cloud2Pos.first, cloud2Pos.second);
+        cloud3.PlaceSprite(cloud3Pos.first, cloud3Pos.second);
     }
 };
