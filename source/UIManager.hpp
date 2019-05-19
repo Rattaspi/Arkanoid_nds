8279befeefd3d36d96679dfda54255ed687f40b0
Alex Canut <a.canutperdigon@gmail.com>
2019-05-19 23:15:31 +0200

    Trying to add my name to the sub screen using the 8x8 spritesheet, but it doesnt work either.

--- source/UIManager.hpp
+++ source/UIManager.hpp
@@ -6,6 +6,8 @@ class UIManager{
     static const int maxHP = 2;
     Sprite boxes[maxHP];
     Sprite hpSprites[maxHP];
+    static const int nameTileLength = 16;
+    Sprite name[nameTileLength];
     
     public:
     
@@ -16,13 +18,27 @@ class UIManager{
     }
 
     void Start(){
+        std::pair<int,int> nameInitialPos = {150, 160};
+
         for(int i = 0; i < maxHP; i++){
             boxes[i] = Sprite(3, 10 + i*12, SCREEN_HEIGHT - 15, false);
             hpSprites[i] = Sprite(1, 12 + i*12, SCREEN_HEIGHT - 13, false);
         }
+
+        for(int row = 0; row < 2; row++){
+            for(int col = 0; col < 8; col++){
+                name[row*col + col] = Sprite(4 + row*col+col, nameInitialPos.first +8*col, nameInitialPos.second + 8*row, false);
+            }
+        }
     }
 
     void Draw(){
+        //NAME DISPLAY
+        for(int i = 0; i<nameTileLength; i++){
+            name[i].PlaceSprite();
+        }
+
+        //HP DISPLAY
         for(int i = 0; i < maxHP; i++){
             boxes[i].PlaceSprite();
             if(i >= hp){
